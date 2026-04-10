28c76f1d3c3531015aeab21522f3ce21d3942267
incorporations-ui <incorporations@bizexpress.in>
2026-04-11 01:49:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { remotePatterns: [] },
-}
-module.exports = nextConfig
+
+  images: {
+    remotePatterns: [],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/inventory/packing',
+        destination: '/sales/packing',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
